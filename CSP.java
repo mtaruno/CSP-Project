ffd05b58a9836196bab00ebb1bddec0ee90cd55d
Blackmoist <emma@dhcp-10-4-9-43.wireless.rochester.edu>
2019-02-26 12:33:56 -0500

    change 1

---
+++ CSP.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public abstract class CSP {
+	protected ArrayList<Variable> var;
+	protected ArrayList<Constraint> constraints;
+	
+	public CSP(ArrayList<Variable> v) {
+		var = v;
+	}
+	
+	public CSP(ArrayList<Variable> v, ArrayList<Constraint> c) {
+		var = v;
+		constraints = c;
+	}
+	
+	public boolean isComplete() {
+		for (Variable v : var) {
+			if (v.assignment() < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public ArrayList<Variable> var() {
+		return var;
+	}
+}
+
+class CSPTest extends CSP {
+
+	public CSPTest(ArrayList<Variable> v) {
+		super(v);
+	}
+	
+	public CSPTest (ArrayList<Variable> v, ArrayList<Constraint> c) {
+		super(v, c);
+	}
+	
+}
