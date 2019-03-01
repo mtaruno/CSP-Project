aaa154727c286721119f4e38b54843c3f922375e
synchronyx <mtaruno@u.rochester.edu>
2019-02-28 23:22:04 -0500

    Update CSPApplication.java

--- CSPApplication.java
+++ CSPApplication.java
@@ -4,10 +4,10 @@ public class CSPApplication {
 	
 	public static void main(String[] args) {
 		
-		AusCSP aus = new AusCSP();
-		Solver solve = new Solver(aus);
-		solve.backTracking(aus);
-		System.out.println(aus.toString());
+//		AusCSP aus = new AusCSP();
+//		Solver solve = new Solver(aus);
+//		solve.backTracking(aus);
+//		System.out.println(aus.toString());
 		
 	}
 }
